7c5dc6d7bd12f67b2157065a4d91a45b2d755f76
pattabhia <shivaram2004@gmail.com>
2022-12-10 20:50:30 +0530

    Initial Commit

---
+++ src/test/java/com/devcodeworld/redis/SpringRedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcodeworld.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
